88d127f93d91cdad13a0f05e59f2970f9a1b7ec3
Giuseppi Pelayo <gpela001@bolt.cs.ucr.edu>
2020-03-09 23:47:23 -0700

    finished lab 7

--- labs/lab-7/main.cpp
+++ labs/lab-7/main.cpp
@@ -1,6 +1,6 @@
-// Name:
-// Quarter, Year:
-// Lab:
+// Name: Giuseppi Pelayo
+// Quarter, Year: Winter, 2020
+// Lab: 07
 //
 // This file is to be modified by the student.
 // main.cpp
@@ -20,20 +20,61 @@
 using namespace std;
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 800;
+vector<vec2> control_points;
 
+float factorial(int n) {
+    int ret = 1;
+    for (int i = n; i > 0; i--) {
+      ret = ret * i;
+    }
+    return ret;
+}
+
+float combination(int n, int k) {
+    return factorial(n) / (factorial(k) * factorial(n-k));
+}
+
+float binomial(int n, int k, float t) {
+    return combination(n, k) * pow(t, k) * pow(1-t, n-k);
+}
 
 void GL_render()
 {
     glClear(GL_COLOR_BUFFER_BIT);
     glutSwapBuffers();
 
-    glBegin(GL_LINES);
+    //glBegin(GL_LINES);
+    glBegin(GL_LINE_STRIP); // use GL_LINE_STRIP to draw line segments between consecutive points
     glColor3f(1.0f,0.0f,0.0f);
-    // just for example, remove if desired
+
+    /* just for example, remove if desired
     glVertex2f(-.5f,-.5f);
     glVertex2f(.5f,-.5f);
     glVertex2f(.5f,.5f);
-    glVertex2f(-.5f,.5f);
+    glVertex2f(-.5f,.5f); */
+
+    if (control_points.size() > 1) {
+      for(float t = 0; t <= 1; t = t + 0.01) { // iterate t between 0 and 1 in steps of 0.01
+        vec2 pt;
+        pt[0] = 0;
+        pt[1] = 0;
+        for (int k = 0; k < (int)control_points.size(); k++) {
+          float mult = binomial(control_points.size() - 1, k, t);
+          pt[0] += (mult * control_points[k][0]);
+          pt[1] += (mult * control_points[k][1]);
+        }
+        glVertex2f(pt[0], pt[1]);
+      }
+    }
+    
+    glEnd();
+    glBegin(GL_POINTS);
+    glColor3f(0.0f,1.0f,0.0f);
+    
+    for (int k = 0; k < (int)control_points.size(); k++) {
+      glVertex2f(control_points[k][0], control_points[k][1]);
+    }
+        
     glEnd();
     glFlush();
 }
@@ -51,6 +92,10 @@ void GL_mouse(int button,int state,int x,int y)
     if(button==GLUT_LEFT_BUTTON && state==GLUT_DOWN){
         double px,py,dummy_z; // we don't care about the z-value but need something to pass in
         gluUnProject(x,y,0,mv_mat,proj_mat,vp_mat,&px,&py,&dummy_z);
+        vec2 pt;
+        pt[0] = px;
+        pt[1] = py;
+        control_points.push_back(pt); // push back the mouse click coordinates into the vector
         glutPostRedisplay();
     }
 }
@@ -64,7 +109,7 @@ void GLInit(int* argc, char** argv)
 
     //glMatrixMode(GL_PROJECTION_MATRIX);
     //glOrtho(0, WINDOW_WIDTH, 0, WINDOW_HEIGHT, -1, 1);
-    glutCreateWindow("CS 130 - <Insert Name Here>");
+    glutCreateWindow("CS 130 - window");
     glutDisplayFunc(GL_render);
     glutMouseFunc(GL_mouse);
 }
